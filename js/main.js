7ee637d8846da2eaefe4549ec7025fa1c79a3951
youngentry <gentry_@naver.com>
2022-08-23 17:41:24 +0900

    220823

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const MainSlider = new Swiper(".MainSlider", {
+    const mainSlider = new Swiper(".mainSlider", {
         loop: true,
         autoplay: {
             delay: 2500,
@@ -10,4 +10,36 @@ window.addEventListener("DOMContentLoaded", () => {
             clickable: true,
         },
     });
+
+    const mainSlogan = new Swiper(".mainSlogan", {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+
+    mainSlider.controller.control = mainSlogan;
+
+    // const smartP = document.querySelector(".Smart p:nth-child(1)");
+    // smartP.addEventListener("click", function () {
+    //     this.classList.add("on");
+    // });
+
+    const smartP = document.querySelectorAll(".Smart .left>p");
+    let idx = 0;
+    smartP.forEach((i, index) =>
+        i.addEventListener("click", (e) => {
+            idx = index;
+            smartP.forEach((i) => {
+                i.classList.remove("on");
+            });
+            i.classList.add("on");
+
+            const txt = document.querySelectorAll(".Smart .txt");
+            txt.forEach((j) => j.classList.remove("on"));
+            let txtIdx = txt[idx];
+            txtIdx.classList.add("on");
+        })
+    );
 });
