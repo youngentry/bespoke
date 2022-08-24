f3c1a04cd49ab6181f07a7e029dc9fae624c4f74
youngentry <gentry_@naver.com>
2022-08-24 17:50:01 +0900

    220824

--- js/main.js
+++ js/main.js
@@ -10,33 +10,26 @@ window.addEventListener("DOMContentLoaded", () => {
             clickable: true,
         },
     });
-
-    const mainSlogan = new Swiper(".mainSlogan", {
-        loop: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-    });
-
-    mainSlider.controller.control = mainSlogan;
-
-    // const smartP = document.querySelector(".Smart p:nth-child(1)");
-    // smartP.addEventListener("click", function () {
-    //     this.classList.add("on");
+    // const mainSlogan = new Swiper(".mainSlogan", {
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 2500,
+    //         disableOnInteraction: false,
+    //     },
+    //     touchRatio: 0,
     // });
 
+    // mainSlider.controller.control = mainSlogan;
+
     const smartP = document.querySelectorAll(".Smart .left>p");
-    // let idx = 0;
+    const txt = document.querySelectorAll(".Smart .txt");
     smartP.forEach((i, idx) =>
         i.addEventListener("click", (e) => {
-            // idx = index;
             smartP.forEach((i) => {
                 i.classList.remove("on");
             });
             i.classList.add("on");
 
-            const txt = document.querySelectorAll(".Smart .txt");
             txt.forEach((j) => j.classList.remove("on"));
             let txtIdx = txt[idx];
             txtIdx.classList.add("on");
