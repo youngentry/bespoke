a93529323e76d2e33f8ff7813f7915b25349e37e
youngentry <90388461+youngentry@users.noreply.github.com>
2022-08-25 22:08:54 +0900

    220825

--- js/main.js
+++ js/main.js
@@ -35,4 +35,26 @@ window.addEventListener("DOMContentLoaded", () => {
             txtIdx.classList.add("on");
         })
     );
+
+    const storySlider = new Swiper(".storySlider", {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "2",
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 500,
+            modifier: 1,
+            slideShadows: false,
+        },
+        // pagination: {
+        //     el: ".swiper-pagination",
+        // },
+    });
 });
