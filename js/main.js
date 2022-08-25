09c2b19d3721d6c1c396fa3c8d4446e4ab8119ed
youngentry <90388461+youngentry@users.noreply.github.com>
2022-08-25 18:52:07 +0900

    Revert "Merge branch 'main' of https://github.com/youngentry/bespoke"
    This reverts commit c80139dcfc0c18cb7f5f5d9ece4561343d86ddb9, reversing
    changes made to 610e3d5987f25935379c2d810cd4fb04807b1b97.

--- js/main.js
+++ js/main.js
@@ -10,26 +10,33 @@ window.addEventListener("DOMContentLoaded", () => {
             clickable: true,
         },
     });
-    // const mainSlogan = new Swiper(".mainSlogan", {
-    //     loop: true,
-    //     autoplay: {
-    //         delay: 2500,
-    //         disableOnInteraction: false,
-    //     },
-    //     touchRatio: 0,
+
+    const mainSlogan = new Swiper(".mainSlogan", {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+
+    mainSlider.controller.control = mainSlogan;
+
+    // const smartP = document.querySelector(".Smart p:nth-child(1)");
+    // smartP.addEventListener("click", function () {
+    //     this.classList.add("on");
     // });
 
-    // mainSlider.controller.control = mainSlogan;
-
     const smartP = document.querySelectorAll(".Smart .left>p");
-    const txt = document.querySelectorAll(".Smart .txt");
-    smartP.forEach((i, idx) =>
+    let idx = 0;
+    smartP.forEach((i, index) =>
         i.addEventListener("click", (e) => {
+            idx = index;
             smartP.forEach((i) => {
                 i.classList.remove("on");
             });
             i.classList.add("on");
 
+            const txt = document.querySelectorAll(".Smart .txt");
             txt.forEach((j) => j.classList.remove("on"));
             let txtIdx = txt[idx];
             txtIdx.classList.add("on");
